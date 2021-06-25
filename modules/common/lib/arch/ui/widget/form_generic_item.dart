31a6b738a2abac1094baeb2f67952df40c18e373
Aliffiro <fathf48@gmail.com>
2021-06-25 14:41:14 +0700

    Add CheckGroup widget implementation

--- modules/common/lib/arch/ui/widget/form_generic_item.dart
+++ modules/common/lib/arch/ui/widget/form_generic_item.dart
@@ -87,10 +87,76 @@ class RadioGroup extends StatelessWidget {
 //TODO: lanjutkan
 class CheckGroup extends StatelessWidget {
   final FormUiCheck itemData;
-  CheckGroup(this.itemData);
+  //TODO: `isValid` blum kepake.
+  final LiveData<bool>? isValid;
+  final MutableLiveData<Set<int>> selectedIndicesLiveData;
+  final bool isLiveDataOwner;
+  final String invalidMsg;
+
+  CheckGroup({
+    required this.itemData,
+    this.isValid,
+    this.invalidMsg = Strings.field_can_not_be_empty,
+    MutableLiveData<Set<int>>? selectedIndicesLiveData,
+    bool? isLiveDataOwner,
+  }):
+    this.selectedIndicesLiveData = selectedIndicesLiveData ?? MutableLiveData(),
+    this.isLiveDataOwner = isLiveDataOwner ?? selectedIndicesLiveData == null
+  ;
 
   @override
   Widget build(BuildContext context) {
+    final optionWidgetList = <Widget>[];
 
+    if(isLiveDataOwner) {
+      final selectedAnswerIndices = itemData.selectedAnswers;
+      selectedIndicesLiveData.value = selectedAnswerIndices;
+    }
+
+    for(int i = 0; i < itemData.answerItems.length; i++) {
+      final option = itemData.answerItems[i];
+      optionWidgetList.add(
+        Flexible(
+          child: ListTile(
+            title: Text(option),
+            leading: LiveDataObserver<Set<int>>(
+              isLiveDataOwner: isLiveDataOwner,
+              liveData: selectedIndicesLiveData,
+              builder: (ctx, data) => Checkbox(
+                value: selectedIndicesLiveData.value!.contains(i),
+                onChanged: (isSelected) => isSelected == true
+                    ? selectedIndicesLiveData.value!.add(i)
+                    : selectedIndicesLiveData.value!.remove(i),
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+
+    final optionsWidget = Column(
+      //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: optionWidgetList,
+    );
+
+    final outerColumChildren = <Widget>[
+      Text(itemData.question),
+      optionsWidget,
+    ];
+
+    if(isValid != null) {
+      outerColumChildren.insert(1, LiveDataObserver<bool>(
+        liveData: isValid!,
+        builder: (ctx, isValid) => isValid == false
+            ? Text(
+          invalidMsg,
+          style: SibTextStyles.size_min_2.copyWith(color: red),
+        ) : SizedBox(),
+      ));
+    }
+
+    return Column(
+      children: outerColumChildren,
+    );
   }
 }
